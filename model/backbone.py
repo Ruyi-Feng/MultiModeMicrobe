80257829a400a039660f51ad70bf7b8bfb95d5bb
Ruyi-Feng <fengruyi@seu.edu.cn>
2025-10-01 21:58:49 +0800

    fix: alignment

--- model/backbone.py
+++ model/backbone.py
@@ -1,33 +1,24 @@
 
+import torch
 import torch.nn as nn
 
+class Alignment(nn.Module):
+    def __init__(self, aa_encoder, property_encoder, trainable: dict,
+                 cross_hidden_size: int = 128, output_hidden_states=True):
+        super(Alignment, self).__init__()
 
-class Microbe(nn.Module):
-    def __init__(self, aa_encoder, property_encoder, llm_decoder, trainable: dict,
-                 cross_hidden_size: int = 128):
-        super(Microbe, self).__init__()
-
-        self._grad_adjustment(aa_encoder, trainable['aa_encoder'])
-        self._grad_adjustment(property_encoder, trainable['property_encoder'])
-        self._grad_adjustment(llm_decoder, trainable['llm_decoder'])
+        grad_adjustment(aa_encoder, trainable['aa_encoder'])
+        grad_adjustment(property_encoder, trainable['property_encoder'])
 
         self.aa_encoder = aa_encoder
         self.property_encoder = property_encoder
-        self.llm_decoder = llm_decoder
         self._aa_proj = nn.Linear(aa_encoder.embedding_dim, cross_hidden_size, bias=False)
         self._property_proj = nn.Linear(property_encoder.embedding_dim, cross_hidden_size, bias=False)
-        self._llm_proj = nn.Linear(cross_hidden_size, llm_decoder.in_features, bias=False)
-        self._alignment = CrossAttention(cross_hidden_size, num_heads=8, dropout=0.1)
+        self._alignment = CrossAttentionFusion(cross_hidden_size, num_heads=8, dropout=0.1)
+        self.output_hidden_states = output_hidden_states
 
-    def _grad_adjustment(self, model, required_grad):
-        if required_grad == True:
-            for param in model.parameters():
-                param.requires_grad = True
-        else:
-            for param in model.parameters():
-                param.requires_grad = False
-
-        return
+        self.cls_token = nn.Parameter(torch.zeros(1, 1, cross_hidden_size))
+        nn.init.normal_(self.cls_token, std=0.02)  # 初始化
 
     def forward(self, aa_seq, property_seq):
         aa_embedding = self.aa_encoder(aa_seq)   # B, S, H_a
@@ -35,12 +26,43 @@ class Microbe(nn.Module):
         property_embedding = self.property_encoder(property_seq)
         property_embedding = self._property_proj(property_embedding)
 
-        crossed_embedding = self._alignment(aa_embedding, property_embedding, property_embedding)[0]
-        cross_embedding = self._llm_proj(crossed_embedding)
+        B = property_embedding.size(0)
 
-        llm_decoding = self.llm_decoder(cross_embedding)
-        return llm_decoding
+        # 将可学习的 [CLS] token 扩展到 batch 维度，并拼接到 property 序列开头
+        cls_tokens = self.cls_token.expand(B, -1, -1)  # (1,1,H) -> (B,1,H)
+        property_embedding = torch.cat([cls_tokens, property_embedding], dim=1)  # (B, S_p+1, H)
+        crossed_embedding = self._alignment(property_embedding, aa_embedding)
 
+        cls_output = crossed_embedding[:, 0, :]  # (B, H)
+
+        if self.output_hidden_states:
+            return cls_output, crossed_embedding  # 可选：返回完整序列
+        else:
+            return cls_output  # 直接返回 [CLS] 表示
+
+
+class CrossAttentionFusion(nn.Module):
+    def __init__(self, embed_dim, num_heads, dropout):
+        super().__init__()
+        self.cross_attn = CrossAttention(embed_dim, num_heads, dropout)
+        self.norm = nn.LayerNorm(embed_dim)
+        self.ffn = nn.Sequential(
+            nn.Linear(embed_dim, embed_dim * 4),
+            nn.GELU(),
+            nn.Linear(embed_dim * 4, embed_dim)
+        )
+
+    def forward(self, text_emb, aa_emb):
+        # text_emb: (B, L_t, D)
+        # image_emb: (B, L_v, D)
+        attn_out, _ = self.cross_attn(
+            query=text_emb,    # Query
+            key=aa_emb,     # Key
+            value=aa_emb    # Value
+        )
+        out = self.norm(text_emb + attn_out)
+        out = self.norm(out + self.ffn(out))
+        return out  # 对齐后的文本表示
 
 class CrossAttention(nn.Module):
     def __init__(self, embed_dim, num_heads, dropout):
@@ -66,3 +88,30 @@ class CrossAttention(nn.Module):
             need_weights=False
         )
         return attn_output, attn_weights
+
+class LLMDecoder(nn.Module):
+    def __init__(self, head_module, llm_decoder, trainable: dict, cross_hidden_size: int = 128):
+        super(LLMDecoder, self).__init__()
+
+        grad_adjustment(llm_decoder, trainable['llm_decoder'])
+        self.head_module = head_module
+        self.llm_decoder = llm_decoder
+        self._llm_proj = nn.Linear(cross_hidden_size, llm_decoder.config.hidden_size, bias=False)
+
+    def forward(self, aa_seq, property_seq):
+        crossed_embedding = self.head_module(aa_seq, property_seq)[0]
+
+        cross_embedding = self._llm_proj(crossed_embedding)
+        llm_decoding = self.llm_decoder(cross_embedding)
+        pass
+
+
+def grad_adjustment(model, required_grad):
+    if required_grad == True:
+        for param in model.parameters():
+            param.requires_grad = True
+    else:
+        for param in model.parameters():
+            param.requires_grad = False
+
+    return
