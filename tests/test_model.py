27a6316d21c19933ce1a123521467214cd164ccd
Ruyi-Feng <fengruyi@seu.edu.cn>
2025-10-02 13:48:43 +0800

    fix: tests

--- tests/test_model.py
+++ tests/test_model.py
@@ -20,20 +20,44 @@ def test_backbone():
 
 
 def test_load_qwen_decoder():
+    aa_encoder, alphabet = esm.pretrained.esm2_t33_650M_UR50D()
+    aa_encoder = aa_encoder.to("cuda")
+    trainable = {'aa_encoder': False, 'property_encoder': False, 'llm_decoder': True}
+
     model_path = "Qwen/Qwen-1_8B"
-    tokenizer = AutoTokenizer.from_pretrained(model_path, trust_remote_code=True)
-    model = AutoModelForCausalLM.from_pretrained(
+    property_tokenizer = AutoTokenizer.from_pretrained(model_path, trust_remote_code=True)
+
+    property_tokenizer.pad_token = "<|endoftext|>"
+    property_tokenizer.pad_token_id = property_tokenizer.convert_tokens_to_ids("<|endoftext|>")
+    property_encoder = AutoModelForCausalLM.from_pretrained(
         model_path,
         device_map="auto",
         trust_remote_code=True
-    ).eval()
-    inputs = tokenizer(
-    "菌株属性有xxx",
-      return_tensors='pt')
+    )
+    property_encoder = property_encoder.to("cuda")
 
-    inputs = inputs.to(model.device)
-    pred = model(**inputs, output_hidden_states=True, output_attentions=False, return_dict=True)
-    last_hidden_states = pred.hidden_states[-1]
+    aa_seq = torch.randint(0, 9, (4, 10)).to("cuda")   # batch = 4, seq_len = 10
+    descriptions = [
+        "Domain: Bacteria, Phylum: Bacillota, Class: Bacilli, ",
+        "Domain: Bacteria, Phylum: Proteobacteria, Class: Gammaproteobacteria, ",
+        "Domain: Archaea, Phylum: Euryarchaeota, Class: Methanomicrobia, ",
+        "Domain: Bacteria, Phylum: Actinobacteriota, Class: Actinobacteria, "
+    ]
+    property_seq = property_tokenizer(
+        descriptions,
+        return_tensors='pt',            # 返回 PyTorch tensor
+        padding=True,                   # 自动 padding 到最长序列
+        truncation=True,                # 超长截断
+        max_length=512                  # 可选
+    )
+
+    property_seq = {k: v.to(property_encoder.device) for k, v in property_seq.items()}
+
+    microbe = Microbe(aa_encoder, 33, property_encoder, trainable, cross_hidden_size=512)
+    microbe = microbe.to(property_encoder.device)
+    pred = microbe(aa_seq, property_seq)
+
+    assert pred.shape == (4, 1)
 
 
 def test_esm2_as_aaencoder():
