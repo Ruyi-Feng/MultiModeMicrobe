8caffc2bf4a653d3225080f02facf88947370ae2
Ruyi-Feng <fengruyi@seu.edu.cn>
2025-10-21 18:54:29 +0800

    fix: dataset parameters

--- config/parameters.py
+++ config/parameters.py
@@ -3,7 +3,7 @@ import argparse
 
 def data_provider_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--model_name', type=str, default='esm2_t30_150M_UR50D')
+    parser.add_argument('--model_name', type=str, default='esm2_t12_35M_UR50D')
     parser.add_argument('--device', type=str, default='cuda')
     parser.add_argument('--batch_size', type=int, default=1)
     parser.add_argument('--cut_off', type=int, default=512)
@@ -14,6 +14,14 @@ def data_provider_args():
 def train_args():
     parser = argparse.ArgumentParser()
     parser.add_argument('--device', type=str, default='cuda')
+    parser.add_argument('--index_path', type=str, default='./data/index.txt')
+    parser.add_argument('--data_path', type=str, default='./data/')
+    parser.add_argument('--model_name', type=str, default='esm2_t12_35M_UR50D')
+    parser.add_argument('--batch_size', type=int, default=2)
+    parser.add_argument('--cut_off', type=int, default=512)
+    parser.add_argument('--collective', action='store_true', default=False)
+    parser.add_argument('--lr', type=float, default=1e-5)
+    parser.add_argument('--epoch', type=int, default=5)
 
     args = parser.parse_args()
     return args
